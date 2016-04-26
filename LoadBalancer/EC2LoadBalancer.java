33f11fbe99ead3a0f49ee4b7d04648add1b44281
Grazina <goncalo.grazina@gmail.com>
2016-04-26 22:44:19 +0100

    Load Balancer now updates the "Running Instances" list periodically

--- LoadBalancer/EC2LoadBalancer.java
+++ LoadBalancer/EC2LoadBalancer.java
@@ -1,5 +1,3 @@
-
-    
 import java.io.*;
 import java.net.*;
 import java.util.HashMap;
@@ -8,26 +6,20 @@ import java.util.ArrayList;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.HttpEntity;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
 import com.amazonaws.services.ec2.model.Instance;
 import java.nio.charset.StandardCharsets;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import com.sun.net.httpserver.*;
- 
- 
- 
+
 public class EC2LoadBalancer {
 
 	private static ArrayList<Instance> instances;
 	private static int next = 0;
+    private static int TIME_TO_REFRESH_INSTANCES = 5000;
+    private static Timer timer = new Timer();
  
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
@@ -35,14 +27,14 @@ public class EC2LoadBalancer {
         server.setExecutor(null); // creates a default executor
         createInstanceList();
         server.start();
+        startTimer();
     }
 
     public static void createInstanceList(){
-    	EC2GeneralOperations ecgo = new EC2GeneralOperations();
     	
     	try{
-        	ecgo.init();
-        	instances = ecgo.getInstances();
+        	EC2GeneralOperations.init();
+        	instances = EC2GeneralOperations.getInstances();
         	System.out.println(instances.toString());
         	next = 0;
     	}catch(Exception e){
@@ -67,25 +59,15 @@ public class EC2LoadBalancer {
     static class MyHandler implements HttpHandler {
         public void handle(final HttpExchange exchange) throws IOException {
  
-        
-        
         new Thread(new Runnable(){
-        	
-        	
+
 			//@Override
 			public void run() {
-				
-				InputStream inpStream = exchange.getRequestBody();
 				Headers responseHeaders = exchange.getResponseHeaders();
 				responseHeaders.set("Content-Type", "text/html");
-				       
-				
 				HashMap map = queryToMap(exchange.getRequestURI().getQuery());
-			
-				
-			    
+
 			  try{
-				
 			        String url = "http://"+instances.get(next).getPublicIpAddress()+":8000/f.html?n="+map.get("n");
 			        System.out.print("Next id: "+next+"\n");
 			        if (next >= (instances.size()-1)){
@@ -94,15 +76,7 @@ public class EC2LoadBalancer {
 			            next += 1;
 			        }
 
-				
-					//String url = "http://52.11.195.145:8000/f.html?n="+map.get("n");
-					
-					
 					System.out.print(url+"\n");
-					
-					//System.out.println("Thread Id: "+Thread.currentThread().getId());
-	
-					
 					HttpClient client = HttpClientBuilder.create().build();
 					HttpGet request = new HttpGet(url);
 	
@@ -115,9 +89,8 @@ public class EC2LoadBalancer {
 					BufferedReader rd = new BufferedReader(
 						new InputStreamReader(response.getEntity().getContent(),StandardCharsets.UTF_8));
 	
-					StringBuffer result = new StringBuffer();
-					String line = "";
-					//result.append("~(º~º)-|_200_| ");
+					StringBuilder result = new StringBuilder();
+					String line;
 					while ((line = rd.readLine()) != null) {
 						result.append(line);
 					}
@@ -128,11 +101,26 @@ public class EC2LoadBalancer {
 			        os.close();
 			        
 		        }catch(Exception e){}
-			        
-				
 			}
 		}).start();
         }
+    }
+
+    public static void startTimer(){
+        // scheduling the task at interval
+
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                updateRunningInstances();
+            }
+        }, TIME_TO_REFRESH_INSTANCES, TIME_TO_REFRESH_INSTANCES);
+    }
+
+    public static void updateRunningInstances(){
+        instances = null;
+        instances = EC2GeneralOperations.getRunningInstancesArray();
+        System.out.println("Running instances: "+instances.size());
     }
  
 }
